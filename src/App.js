1af23083b417a8fe9a2a3a2f0ae6c1c7dc436e37
L. Yasril Imam <60500658+yasrilimam98@users.noreply.github.com>
2022-08-24 21:42:11 +0700

    add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
+import Home from "./pages/Home";
+import Kontak from "./pages/Kontak";
+import Nav from "./pages/Nav";
+import Sejarah from "./pages/Sejarah";
+import Tentang from "./pages/Tentang";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Belajar React</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/kontak" component={Kontak} />
+          <Route path="/sejarah" component={Sejarah} />
+          <Route path="/tentang" component={Tentang} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Nav() {
   return (
     <div className="App">
       <ul>
-        <li>Home</li>
-        <li>Sejarah</li>
-        <li>Tentang</li>
-        <li>Kontak</li>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/sejarah">
+          <li>Sejarah</li>
+        </Link>
+        <Link to="/tentang">
+          <li>Tentang</li>
+        </Link>
+        <Link to="/kontak">
+          <li>Kontak</li>
+        </Link>
       </ul>
     </div>
   );
